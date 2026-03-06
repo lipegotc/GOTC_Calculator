58fd9e8926d3103582ef6d74e6a23f943b2b4003
Felipe <felipecunhacalegari@gmail.com>
2026-03-05 23:46:35 -0300

    small change

--- landing.py
+++ landing.py
@@ -22,7 +22,7 @@ Calculators currently available:
 
 Most inputs have default values so you don't have to type out every single value, adjust as needed.
 
-This is an unofficial fan-made tool and is not affiliated with the game developers. All rights reserved to their respective owners.
+This is an unofficial fan-made tool and is not affiliated with WB Games. All rights reserved to their respective owners.
             
 Questions or suggestions? Message me on Discord (Lipeeeee).
 
